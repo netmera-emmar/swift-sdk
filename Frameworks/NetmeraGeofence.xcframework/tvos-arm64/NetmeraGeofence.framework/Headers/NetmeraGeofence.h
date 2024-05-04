dd50b3636af0590c1d3b809f50640de7c7df50ee
NetmeraSDK <sdk@netmera.com>
2024-05-04 21:30:18 +0300

    Update frameworks for 4.0.2

---
+++ Frameworks/NetmeraGeofence.xcframework/tvos-arm64/NetmeraGeofence.framework/Headers/NetmeraGeofence.h
@@ -0,0 +1,17 @@
+//
+//  NetmeraGeofence.h
+//  NetmeraGeofence
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetmeraGeofence.
+FOUNDATION_EXPORT double NetmeraGeofenceVersionNumber;
+
+//! Project version string for NetmeraGeofence.
+FOUNDATION_EXPORT const unsigned char NetmeraGeofenceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetmeraGeofence/PublicHeader.h>
+
+
