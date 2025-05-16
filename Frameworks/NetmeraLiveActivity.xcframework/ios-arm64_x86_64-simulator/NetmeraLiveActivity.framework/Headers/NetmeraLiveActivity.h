7cb97bafeff5ff96723e45874a86792654ea271c
Emmar Kardeslik <emmar.kardeslik@netmera.com>
2025-05-16 23:08:22 +0300

    Updated XCFrameworks to version 4.2.0

---
+++ Frameworks/NetmeraLiveActivity.xcframework/ios-arm64_x86_64-simulator/NetmeraLiveActivity.framework/Headers/NetmeraLiveActivity.h
@@ -0,0 +1,16 @@
+//
+//  NetmeraLiveActivity.h
+//  NetmeraLiveActivity
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetmeraLiveActivity.
+FOUNDATION_EXPORT double NetmeraLiveActivityVersionNumber;
+
+//! Project version string for NetmeraLiveActivity.
+FOUNDATION_EXPORT const unsigned char NetmeraLiveActivityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetmeraLiveActivity/PublicHeader.h>
+
+
